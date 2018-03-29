8ad322ada199f4cea003fccf3e8b42b68dc335bb
Frank Rose <frankrosework@gmail.com>
2018-03-29 03:24:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ var recipes = {};
 
 var updateObjectWithKeyAndValue = function(object, key, value) {
   return Object.assign({}, object, {[key]: value});
-}
+};
 
 var deleteFromObjectByKey = function(object, key) {
   var obj = Object.assign({}, object);
   delete obj[key];
   return obj;
-}
+};
+
+var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value) {
+  return Object.assign(object, {[key]: value});
+};
+
+var destructivelyDeleteFromObjectByKey = function(object, key) {
+  delete object[key];
+};
